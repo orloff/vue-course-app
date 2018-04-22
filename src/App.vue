5c0f7d355a8f0de86d5faecf5309de72d7b27230
Alex Sokolov <4497128+Alex-Sokolov@users.noreply.github.com>
2018-04-22 21:48:17 +0300

    Добавлен Vuex

--- src/App.vue
+++ src/App.vue
@@ -2,57 +2,59 @@
   <div>
 
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a 
-        class="navbar-brand" 
-        href="#">Vue course app</a>
-      <button 
-        class="navbar-toggler" 
-        type="button" 
-        data-toggle="collapse" 
-        data-target="#navbarCollapse" 
-        aria-controls="navbarCollapse" 
-        aria-expanded="false" 
+      <router-link 
+        to="/" 
+        class="navbar-brand">
+        {{ layoutTitle }}
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
         aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"/>
       </button>
-      <div 
-        class="collapse navbar-collapse" 
+      <div
+        class="collapse navbar-collapse"
         id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
-          <router-link 
-            tag="li" 
-            to="/" 
-            exact 
+          <router-link
+            tag="li"
+            to="/"
+            exact
             active-class="active">
-            <a 
-              class="nav-link" 
+            <a
+              class="nav-link"
               href="#">Главная</a>
           </router-link>
-          <router-link 
-            tag="li" 
-            to="/users" 
-            exact 
+          <router-link
+            tag="li"
+            to="/users"
+            exact
             active-class="active">
-            <a 
-              class="nav-link" 
+            <a
+              class="nav-link"
               href="#">Список пользователей</a>
           </router-link>
-          <router-link 
-            tag="li" 
-            to="/add" 
-            exact 
+          <router-link
+            tag="li"
+            to="/add"
+            exact
             active-class="active">
-            <a 
-              class="nav-link" 
+            <a
+              class="nav-link"
               href="#">Добавить пользователя</a>
           </router-link>
         </ul>
       </div>
     </nav>
 
-    <main 
-      id="app" 
-      role="main" 
+    <main
+      id="app"
+      role="main"
       class="container">
       <router-view/>
     </main>
@@ -64,7 +66,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    // Заголовок страницы из хранилища Vuex
+    // который будет на всех страницах
+    layoutTitle() {
+      return this.$store.state.navbarTitle
+    }
+  }
 }
 </script>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,11 +8,35 @@
     <router-link to="/users">
       Открыть список пользователей
     </router-link>
+
+    <hr>
+
+    <div class="form-group">
+      <label>Поле формы привязанное к Vuex</label>
+      <input
+        type="text"
+        class="form-control"
+        v-model="vuextest"
+      >
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    // Двустороннее вычисляемое свойство
+    // для считывания и сохранения значения в Vuex
+    vuextest: {
+      get() {
+        return this.$store.state.navbarTitle
+      },
+      set(value) {
+        this.$store.commit('changeTitle', value)
+      }
+    }
+  }
 }
 </script>
